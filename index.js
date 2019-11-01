eeeed748feea39cb6fe37e7e01166e80c841b58a
Jen Wachter <jwachter@jhu.edu>
2019-11-01 14:28:52 -0400

    Added polyfill package instead of embedded code

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-/**
- * IE pollyfill for CustomEvent constructor
- * https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
- */
-(function () {
-
-  if ( typeof window.CustomEvent === "function" ) return false;
-
-  function CustomEvent ( event, params ) {
-    params = params || { bubbles: false, cancelable: false, detail: null };
-    var evt = document.createEvent( 'CustomEvent' );
-    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
-    return evt;
-   }
-
-  window.CustomEvent = CustomEvent;
-})();
-
-
+require('custom-event-polyfill');
 var throttle = require('lodash.throttle');
 
 var WindowResizeWatcher = function (namespace) {
